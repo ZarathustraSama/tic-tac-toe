2bfd71360e3206407ce5773c4c7f731199fc34c2
ZarathustraSama <mr.gabriel@live.it>
2024-01-11 11:42:03 +0100

    #reduce_grid tests implemented

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -4,8 +4,6 @@ require_relative '../lib/grid.rb'
 
 describe Grid do
   describe '#reduce_grid' do
-    let(:player_x) { 'X' }
-    let(:player_o) { 'O' }
 
     context 'at the beginning of the game' do
       subject(:grid_start) { described_class.new }
@@ -14,6 +12,23 @@ describe Grid do
         expect(grid_start.reduce_grid(player_x)).to eql(0)
       end
     end
+
+    context 'when there are 3 "X" and 4 "O"' do
+      subject(:grid_mid) { described_class.new([['X', 'O', 'X'], ['X', 'O', 'O'], ['O', nil, nil]]) }
+
+      it 'returns 3 when asked for X' do
+        expect(grid_mid.reduce_grid('X')).to eql(3)
+      end
+
+      it 'returns 4 when asked for O' do
+        expect(grid_mid.reduce_grid('O')).to eql(4)
+      end
+    end
   end
+
+  describe '#get_current_player' do
+    
+  end
+
 
 end
