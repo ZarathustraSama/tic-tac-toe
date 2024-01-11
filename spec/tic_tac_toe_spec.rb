b70710e7d1211de434470ccae4c2ed08139a680d
ZarathustraSama <mr.gabriel@live.it>
2024-01-11 09:56:38 +0100

    Initial commit

---
+++ spec/tic_tac_toe_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../lib/game.rb'
+require_relative '../lib/grid.rb'
+require_relative '../lib/play.rb'
+
+describe Grid do
+  subject(:grid) { described_class.new }
+  
+end
