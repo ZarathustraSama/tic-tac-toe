3a2ca40bd968e697fb10fe234fcd8fe30ace78b8
ZarathustraSama <mr.gabriel@live.it>
2024-01-11 11:11:04 +0100

    Changed logic on how methods access the grid

--- lib/grid.rb
+++ lib/grid.rb
@@ -12,25 +12,25 @@ class Grid
     @grid = grid
   end
 
-  def get_current_player(grid)
-    if grid == initial_state
+  def get_current_player
+    if @grid == initial_state
       return X
-    elsif game_over?(grid)
+    elsif game_over?
       return
     end
 
-    reduce_grid(grid, X) > reduce_grid(grid, O) ? O : X
+    reduce_grid(X) > reduce_grid(O) ? O : X
   end
       
   # Helper function to check how many x/o are in the grid
-  def reduce_grid(grid, player)
+  def reduce_grid(player)
     i = 0
-    grid.each { |row| row.each { |cell| i += 1 if cell == player } }
+    @grid.each { |row| row.each { |cell| i += 1 if cell == player } }
     i
   end
 
-  def get_winner(grid)
-    winner = get_winner_row(grid) || get_winner_column(grid) || get_winner_diagonal(grid)
+  def get_winner
+    winner = get_winner_row|| get_winner_column || get_winner_diagonal
     return winner if winner
 
     nil
@@ -38,51 +38,51 @@ class Grid
 
   # After the game over, checks for rows, columns and diagonals
   # If none won, returns nil (it's a tie)
-  def get_winner_row(grid)
-    grid.each { |row| return row[0] if row.uniq.count == 1 && row[0] != EMPTY }
+  def get_winner_row
+    @grid.each { |row| return row[0] if row.uniq.count == 1 && row[0] != EMPTY }
     nil
   end
 
-  def get_winner_column(grid)
-    grid.each do |row|
+  def get_winner_column
+    @grid.each do |row|
       row.each_with_index do |cell, i|
-        return cell if [grid[0][i], grid[1][i], grid[2][i]].uniq.count == 1 && i <= 2 && cell != EMPTY
+        return cell if [@grid[0][i], @grid[1][i], @grid[2][i]].uniq.count == 1 && i <= 2 && cell != EMPTY
       end
     end
     nil
   end
   
-  def get_winner_diagonal(grid)
-    return grid[1][1] if eql_diagonal?(grid) && grid[1][1] != EMPTY
+  def get_winner_diagonal
+    return @grid[1][1] if eql_diagonal? && @grid[1][1] != EMPTY
 
     nil
   end
 
-  def eql_diagonal?(grid)
-    left_diagonal = [grid[0][0], grid[1][1], grid[2][2]]
-    right_diagonal = [grid[0][2], grid[1][1], grid[2][0]]
+  def eql_diagonal?
+    left_diagonal = [@grid[0][0], @grid[1][1], @grid[2][2]]
+    right_diagonal = [@grid[0][2], @grid[1][1], @grid[2][0]]
     left_diagonal.uniq.count == 1 || right_diagonal.uniq.count == 1
   end
 
-  def game_over?(grid)
-    return true if get_winner(grid)
-    return false if grid.flatten.any?(nil)
+  def game_over?
+    return true if get_winner
+    return false if @grid.flatten.any?(nil)
 
     true
   end
 
-  def make_move(move, grid)
-    grid[move[0]][move[1]] = get_current_player(grid)
+  def make_move(move)
+    @grid[move[0]][move[1]] = get_current_player
     self
   end
 
-  def draw_grid(grid)
+  def draw_grid
     puts("\n")
-    draw_row(grid[0])
+    draw_row(@grid[0])
     puts("\n-----------")
-    draw_row(grid[1])
+    draw_row(@grid[1])
     puts("\n-----------")
-    draw_row(grid[2])
+    draw_row(@grid[2])
     puts("\n\n")
   end
 
--- lib/play.rb
+++ lib/play.rb
@@ -8,14 +8,14 @@ grid = Grid.new
 game = Game.new
 
 loop do
-  grid.draw_grid(grid.grid)
-  game_over = grid.game_over?(grid.grid)
-  player = grid.get_current_player(grid.grid)
+  grid.draw_grid
+  game_over = grid.game_over?
+  player = grid.get_current_player
   if game_over
-    winner = grid.get_winner(grid.grid)
+    winner = grid.get_winner
     return winner.nil? ? puts('Game Over: It\'s a tie!') : puts("Game Over: #{winner} wins!")
   else
     puts("Player #{player}'s turn")
   end
-  grid = grid.make_move(game.ask_user_move(grid.grid), grid.grid)
+  grid = grid.make_move(game.ask_user_move(grid.grid))
 end
