1077d53fe9cab13981c778b1026e295a555fddb4
ZarathustraSama <mr.gabriel@live.it>
2024-01-12 12:10:45 +0100

    Rearranged into a class for better testing

--- lib/play.rb
+++ lib/play.rb
@@ -3,20 +3,25 @@
 require_relative './grid'
 require_relative './game'
 
-EMPTY = nil
-
-grid = Grid.new
-game = Game.new
-
-loop do
-  grid.draw_grid
-  game_over = grid.game_over?
-  player = grid.get_current_player
-  if game_over
-    winner = grid.get_winner
-    return winner.nil? ? puts('Game Over: It\'s a tie!') : puts("Game Over: #{winner} wins!")
-  else
-    puts("Player #{player}'s turn")
+# Can be played from the command line
+class Play
+  def initialize(grid = Grid.new, game = Game.new)
+    @grid = grid
+    @game = game
   end
-  grid = grid.make_move(game.ask_user_move(grid.grid))
+
+  def play_game
+    loop do
+      @grid.draw_grid
+      game_over = @grid.game_over?
+      player = @grid.get_current_player
+      if game_over
+        winner = @grid.get_winner
+        return winner.nil? ? puts('Game Over: It\'s a tie!') : puts("Game Over: #{winner} wins!")
+      else
+        puts("Player #{player}'s turn")
+      end
+      @grid = @grid.make_move(@game.ask_user_move(@grid.grid))
+    end
+  end
 end
